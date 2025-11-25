54e6b712341ea8608759d9a913a3aadc076c3932
Laith <LaithSama666@gmail.com>
2025-11-25 08:26:46 +0300

    Ajout de l'exo php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo PhP</title>
+    <link rel="stylesheet" href="assets\CSS\style.css">
+    <!-- <link rel="stylesheet" href="assets\CSS\lol.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="logo">
+        <h1>Youtube</h1>
+        <form method="POST">
+            <input type="serarch" placehoder="Rechercher ...">
+            <button type="submit">Rechercher</button>
+        </form>
+        <div class="image">
+            <img src="assets\IMG\images.png" alt="icoYoutube">
+        </div>
+    </div>
+    <div class="conteneur">
+        <nav>
+            <ul>
+                <li><i class="fa-solid fa-house"></i><a href="#">Accueil</a></li>
+                <li><a href="#">Shorts</a></li>
+                <li><a href="#">Abonemments</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="video">
+        <?php 
+                $liste = array("Tous", "Musique", "Mix", "Rap Français");
+                for ($i = 0; $i < count($liste); $i++) {
+                    ?>
+                    <button><?php echo $liste[$i] ?></button>
+                    <?php
+                }
+        ?>
+    </div>
+    
+</body>
+</html>
